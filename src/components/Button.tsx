97787bbd58d8ae8c2525e8fb6b6b395d5fead7f3
Alberto Ortiz <alfaortiz.sis@gmail.com>
2024-10-18 04:02:32 -0400

    Add Nosotros, Como funciona, contacto

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,36 +9,34 @@ interface Props {
   url: string
 }
 
-export default function Button({ children, onClick,url, target, className, ...rest }:Props) {
-  return (
-    
-<a
-  href={url}
-  target={`${target ?? '_blank'}`}
-  rel="noopener noreferrer"
-  onClick={onClick}
-  class={`
-  lg:text-2xl
-  md:px-5
-  md:text-xl
-  text-xs
-  w-fit
-  font-medium
-  no-underline
-  px-3
-    py-3
-    border
-    border-solid
-    border-white
-    rounded-full
-    uppercase
-    ${styles.button}
-    ${className ?? ''}`
-  }
-  {...rest}
->
-  {children}
-</a>
-
+export default function Button({ children, onClick, url, target,  className, ...rest }:Props) {
+  return (     
+    <a
+      href={url}
+      target={`${target ?? '_blank'}`}
+      rel="noopener noreferrer"
+      onClick={onClick}
+      class={`
+        lg:text-2xl
+        md:px-5
+        md:text-xl
+        text-xs
+        w-fit
+        font-medium
+        no-underline
+        px-3
+        py-3
+        border
+        border-solid
+        border-white
+        rounded-full
+        uppercase
+        ${styles.button}
+        ${className ?? ''}`
+        }
+      {...rest}
+    >
+      {children}
+    </a>
   )
 }
