6da55a72b10b6f5d39a97a3798a778f7f89e9562
Alberto Ortiz <alfaortiz.sis@gmail.com>
2025-02-22 10:45:30 -0400

    Versión con Base de Datos en Asto - Autenticación

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,29 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import db from '@astrojs/db';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [
+    tailwind(),
+    preact({
+      include: ["**/preact/*"],
+    }),
+    db(),
+    react({
+      include: ["**/react/*"],
+    }),
+  ],
+  // output: "server",
 
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en', 'pt'],
+    defaultLocale: "es",
+    locales: ["es", "en", "pt"],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-
 });
